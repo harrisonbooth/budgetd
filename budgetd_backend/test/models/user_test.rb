3a227bed64364cbf0bb9c08baf2bd07f9bae92c9
Harrison Booth <harrisonbooth@ymail.com>
2017-03-19 13:29:26 +0000

    add passing user email tests

--- budgetd_backend/test/models/user_test.rb
+++ budgetd_backend/test/models/user_test.rb
@@ -4,4 +4,11 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "User has an email" do
+    assert_equal "one@example.com", users(:one).email
+  end
+
+  test "User email is not hard coded" do
+    assert_equal "two@example.com", users(:two).email
+  end
 end
