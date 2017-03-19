45873c8ffab75ac514cab75965b6f60837417e6e
Harrison Booth <harrisonbooth@ymail.com>
2017-03-19 14:16:06 +0000

    add passing transaction amount tests

--- budgetd_backend/test/models/transaction_test.rb
+++ budgetd_backend/test/models/transaction_test.rb
@@ -8,4 +8,12 @@ class TransactionTest < ActiveSupport::TestCase
   test "Transaction subBudget is not hard coded" do
     assert_equal "Food", transactions(:two).sub_budget.name
   end
+
+  test "Transaction has an amount" do
+    assert_equal 1.60, transactions(:one).amount
+  end
+
+  test "Transaction amount is not hard coded" do
+    assert_equal 32.20, transactions(:two).amount
+  end
 end
