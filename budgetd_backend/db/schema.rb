0ec7a95bbb51cba4986746b77f3632d990dd5bb3
Harrison Booth <harrisonbooth@ymail.com>
2017-03-19 14:11:42 +0000

    migrate transaction model into db

--- budgetd_backend/db/schema.rb
+++ budgetd_backend/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170319135822) do
+ActiveRecord::Schema.define(version: 20170319141114) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,16 @@ ActiveRecord::Schema.define(version: 20170319135822) do
 
   add_index "sub_budgets", ["budget_id"], name: "index_sub_budgets_on_budget_id", using: :btree
 
+  create_table "transactions", force: :cascade do |t|
+    t.integer  "sub_budget_id"
+    t.decimal  "amount"
+    t.string   "location"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  add_index "transactions", ["sub_budget_id"], name: "index_transactions_on_sub_budget_id", using: :btree
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -59,4 +69,5 @@ ActiveRecord::Schema.define(version: 20170319135822) do
 
   add_foreign_key "budgets", "users"
   add_foreign_key "sub_budgets", "budgets"
+  add_foreign_key "transactions", "sub_budgets"
 end
