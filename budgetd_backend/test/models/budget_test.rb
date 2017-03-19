a5f7e97aa6c78c1443e971bb78da1fc7c3a715ea
Harrison Booth <harrisonbooth@ymail.com>
2017-03-19 13:53:03 +0000

    add passiing budget user tests

--- budgetd_backend/test/models/budget_test.rb
+++ budgetd_backend/test/models/budget_test.rb
@@ -4,4 +4,8 @@ class BudgetTest < ActiveSupport::TestCase
   test "Budget has a user" do
     assert_equal "Harrison", budgets(:one).user.firstname
   end
+
+  test "Budget user is not hard coded" do
+    assert_equal "Lynn", budgets(:two).user.firstname
+  end
 end
