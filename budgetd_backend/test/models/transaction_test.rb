fed6f0403fa65f52b8f9e1e4b95c3ae955c43234
Harrison Booth <harrisonbooth@ymail.com>
2017-03-19 14:14:57 +0000

    add passing transaction subbudget tests

--- budgetd_backend/test/models/transaction_test.rb
+++ budgetd_backend/test/models/transaction_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class TransactionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Transaction has a subBudget" do
+    assert_equal "Coffee", transactions(:one).sub_budget.name
+  end
+
+  test "Transaction subBudget is not hard coded" do
+    assert_equal "Food", transactions(:two).sub_budget.name
+  end
 end
