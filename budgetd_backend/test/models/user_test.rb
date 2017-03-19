5e8a2d02ce89c36e6fef3c5b05ba9035a58321bb
Harrison Booth <harrisonbooth@ymail.com>
2017-03-19 13:33:17 +0000

    add passing user password tests

--- budgetd_backend/test/models/user_test.rb
+++ budgetd_backend/test/models/user_test.rb
@@ -4,11 +4,20 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
   test "User has an email" do
     assert_equal "one@example.com", users(:one).email
   end
 
   test "User email is not hard coded" do
     assert_equal "two@example.com", users(:two).email
+  end
+
+  test "User has password" do
+    assert_equal "password", users(:one).encrypted_password
+  end
+
+  test "User password is not hard coded" do
+    assert_equal "password1", users(:two).encrypted_password
   end
 end
