9eeb7d691844d4573ad3f39dd9cf623c5216aa01
Harrison Booth <harrisonbooth@ymail.com>
2017-03-19 14:02:18 +0000

    add passing subbudget amount tests

--- budgetd_backend/test/models/sub_budget_test.rb
+++ budgetd_backend/test/models/sub_budget_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class SubBudgetTest < ActiveSupport::TestCase
   test "SubBudget has amount" do
-    assert_equal 19.99, subBudgets(:one).amount
+    assert_equal 19.99, sub_budgets(:one).amount
+  end
+
+  test "SubBudget amount is not hard coded" do
+    assert_equal 44.21, sub_budgets(:two).amount
   end
 end
