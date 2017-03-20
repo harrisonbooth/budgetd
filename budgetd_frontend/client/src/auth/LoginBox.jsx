bf583262f8938562188ff96fcee1c1f04125cf33
Harrison Booth <harrisonbooth@ymail.com>
2017-03-20 10:13:26 +0000

    add signin/up/out container

--- budgetd_frontend/client/src/auth/LoginBox.jsx
+++ budgetd_frontend/client/src/auth/LoginBox.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link} from 'react-router'
 import SignIn from './SignIn'
 import SignUp from './SignUp'
 import SignOut from './SignOut'
@@ -20,6 +21,45 @@ class LoginBox extends React.Component {
   fetchUser() {
     const request = new XMLHttpRequest()
     request.open('GET', this.props.url + 'users.json')
+    request.setRequestHeader('content-type', 'application/json')
+    request.withCredentials = true
+
+    request.onload = () => {
+      if(request.status === 200){
+        const receivedUser = JSON.parse(request.resonseText)
+        this.setUser(receivedUser)
+      } else if(request.status === 401){
+        this.setUser(null)
+      }
+    }
+
+    request.send(null)
+  }
+
+  componentDidMount() {
+    this.fetchUser()
+  }
+
+  render() {
+    if(this.state.currentUser){
+      const children = <div>
+        <h4>Welcome back, this.state.currentUser.firstname.</h4>
+        <Link className='logged-in-link' to='/budget'>View your budget</Link>
+      </div>
+    } else {
+      const children = <div>
+        <h4>Please sign in</h4>
+        <SignIn url={this.props.url + 'users/sign_in.json'} onSignIn={this.setUser}/>
+        <p>New to Budget'd? Sign up here</p>
+        <SignUp url={this.props.url + 'users.json'} onSignUp={this.setUser}/>
+      </div>
+    }
+
+    return (
+      <div>
+        {children}
+      </div>
+    )
   }
 
 }
