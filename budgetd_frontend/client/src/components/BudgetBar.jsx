85d1dbea076589b4d246e5b841de1d5ed0e7ac23
Harrison Booth <harrisonbooth@ymail.com>
2017-03-21 12:27:41 +0000

    add bar colours

--- budgetd_frontend/client/src/components/BudgetBar.jsx
+++ budgetd_frontend/client/src/components/BudgetBar.jsx
@@ -6,9 +6,27 @@ const BudgetBar = (props) => {
     width: props.maxWidth
   }
 
+  const percentageUsed = 1 - (props.total / props.originalTotal)
+  let date = new Date()
+  const percentageThroughMonth = date.getDate() / 31
+  let colour = "green"
+
+  console.log(percentageUsed);
+  console.log(percentageThroughMonth);
+
+  if(percentageUsed > percentageThroughMonth + 0.2){
+    colour = "red"
+  } else if(percentageUsed < percentageThroughMonth + 0.2 && percentageUsed > percentageThroughMonth - 0.2){
+    colour = "orange"
+  } else {
+    colour = "green"
+  }
+
+  const classes = "budget-bar-progress " + colour
+
   return (
     <div className='budget-bar'>
-      <progress style={barStyle} className="budget-bar-progress" max={props.originalTotal} value={props.total}></progress>
+      <progress style={barStyle} className={classes} max={props.originalTotal} value={props.total}></progress>
     </div>
   )
 }
